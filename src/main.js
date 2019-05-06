f82b006d22ec41d015582529c839853bfb2e632d
mauricioaznar <maurisio01>
2019-05-06 12:40:08 -0500

    0.7.0

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import MauFormInputText from './components/MauFormInputText'
+import MauFormInputNumber from './components/MauFormInputNumber'
 
 const MauComponentsPlugin = {
   install (Vue) {
     Vue.component('MauFormInputText', MauFormInputText)
+    Vue.component('MauFormInputNumber', MauFormInputNumber)
   }
 }
 
