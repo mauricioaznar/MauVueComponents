7ffecbca3fd45e0e624ca01b7d38551c158ad30d
mauricioaznar <maurisio01>
2019-05-10 11:33:38 -0500

    MauFormInputStatic

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import MauFormInputText from './components/MauFormInputText'
 import MauFormInputNumber from './components/MauFormInputNumber'
 import MauFormInputDate from './components/MauFormInputDate'
+import MauFormInputSelectStatic from './components/MauFormInputSelectStatic'
 
 const MauComponentsPlugin = {
   install (Vue) {
     Vue.component('MauFormInputText', MauFormInputText)
     Vue.component('MauFormInputNumber', MauFormInputNumber)
     Vue.component('MauFormInputDate', MauFormInputDate)
+    Vue.component('MauFormInputSelectStatic', MauFormInputSelectStatic)
   }
 }
 
